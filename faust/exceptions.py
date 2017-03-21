f719e4ad73fa5f94cc1cf68f7c5176452b50abbd
Ask Solem <ask@celeryproject.org>
2017-03-20 22:53:01 -0700

    Docstrings for faust.exceptions

--- faust/exceptions.py
+++ faust/exceptions.py
@@ -1,10 +1,13 @@
+"""Exceptions."""
+
+
 class ImproperlyConfigured(Exception):
-    ...
+    """The library is not configured/installed correctly."""
 
 
 class KeyDecodeError(Exception):
-    ...
+    """Error while decoding/deserializing message key."""
 
 
 class ValueDecodeError(Exception):
-    ...
+    """Error while decoding/deserialization message value."""
