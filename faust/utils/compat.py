76db0ea5802acca6525ad0dec85e2e006bac7fbe
Ask Solem <ask@celeryproject.org>
2017-03-27 22:02:32 -0700

    Worker(debug=True) now starts aiomonitor

--- faust/utils/compat.py
+++ faust/utils/compat.py
@@ -1,7 +1,25 @@
 """Compatibility utilities."""
-from typing import AnyStr
+from typing import Any, AnyStr
 
-__all__ = ['want_bytes', 'want_str']
+__all__ = ['DummyContext', 'want_bytes', 'want_str']
+
+
+class DummyContext:
+
+    def __init__(self, *args, **kwargs):
+        ...
+
+    async def __aenter__(self) -> 'DummyContext':
+        return self
+
+    async def __aexit__(self, *exc_info: Any) -> None:
+        ...
+
+    def __enter__(self) -> 'DummyContext':
+        return self
+
+    def __exit__(self, *exc_info) -> Any:
+        ...
 
 
 def want_bytes(s: AnyStr) -> bytes:
--- faust/worker.py
+++ faust/worker.py
@@ -1,40 +1,57 @@
 import asyncio
-from typing import IO, Sequence, Set, Union, cast
+from typing import Any, IO, Sequence, Set, Union, cast
+from .utils.compat import DummyContext
 from .utils.logging import setup_logging
 from .utils.services import Service
+from .utils.imports import SymbolArg, symbol_by_name
 from .types import AppT, ServiceT, SensorT
 
 
 class Worker(Service):
-
+    debug: False
     sensors: Set[SensorT]
     services: Sequence[ServiceT]
+    loglevel: Union[str, int]
+    logfile: Union[str, IO]
 
     def __init__(self, *services: ServiceT,
                  sensors: Sequence[SensorT] = None,
+                 debug: bool = False,
                  loglevel: Union[str, int] = None,
                  logfile: Union[str, IO] = None,
                  logformat: str = None,
                  loop: asyncio.AbstractEventLoop = None) -> None:
         self.services = services
         self.sensors = set(sensors or [])
+        self.debug = debug
         self.loglevel = loglevel
         self.logfile = logfile
         self.logformat = logformat
         super().__init__(loop=loop)
 
     def execute_from_commandline(self, *coroutines):
-        asyncio.gather(
-            *[asyncio.ensure_future(coro, loop=self.loop)
-              for coro in coroutines],
-            loop=self.loop)
-        self.loop.run_until_complete(self.start())
-        try:
-            self.loop.run_forever()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            self.loop.close()
+        with self._monitor():
+            asyncio.gather(
+                *[asyncio.ensure_future(coro, loop=self.loop)
+                for coro in coroutines],
+                loop=self.loop)
+            self.loop.run_until_complete(self.start())
+            try:
+                self.loop.run_forever()
+            except KeyboardInterrupt:
+                pass
+            finally:
+                self.loop.close()
+
+    def _monitor(self) -> Any:
+        if self.debug:
+            try:
+                import aiomonitor
+            except ImportError:
+                pass
+            else:
+                return aiomonitor.start_monitor(loop=self.loop)
+        return DummyContext()
 
     async def on_start(self) -> None:
         if self.loglevel:
