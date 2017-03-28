62a12f9f540244fc54efe265a33b2f6554c495cd
Ask Solem <ask@celeryproject.org>
2017-03-27 20:35:48 -0700

    Removes Array for now

--- faust/__init__.py
+++ faust/__init__.py
@@ -33,7 +33,7 @@ del(_temp)
 del(re)
 
 from .app import App                         # noqa: E402
-from .models import Array, Record            # noqa: E402
+from .models import Record                   # noqa: E402
 from .sensors import Sensor                  # noqa: E402
 from .streams import Stream, topic           # noqa: E402
 from .tables import Table, table             # noqa: E402
@@ -41,6 +41,13 @@ from .types import Event                     # noqa: E402
 from .worker import Worker                   # noqa: E402
 
 __all__ = [
-    'App', 'Array', 'Event', 'Record', 'Sensor',
-    'Stream', 'Table', 'Worker', 'table', 'topic',
+    'App',
+    'Event',
+    'Record',
+    'Sensor',
+    'Stream',
+    'Table',
+    'Worker',
+    'table',
+    'topic',
 ]
