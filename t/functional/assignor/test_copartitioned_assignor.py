72767fbd3c1cce4baea99c404aec572788249115
Vineet Goel <vineet@robinhood.com>
2017-05-09 15:47:05 -0700

    Functional tests for copartitioned assignor

--- t/functional/assignor/test_copartitioned_assignor.py
+++ t/functional/assignor/test_copartitioned_assignor.py
@@ -1,64 +1,129 @@
-import pytest
+import copy
+from collections import Counter
+from hypothesis import assume, given
+from hypothesis.strategies import integers
+from typing import MutableMapping
 from faust.assignor.client_assignment import CopartitionedAssignment
 from faust.assignor.copartitioned_assignor import CopartitionedAssignor
 
 
-_topics = ["foo", "bar", "baz"]
-_num_partitions = 8
+_topics = {"foo", "bar", "baz"}
 
 
-@pytest.mark.parametrize("client_assignments,replicas", [
-    # Fresh assignment with 1 replica
-    ({
-        "cli1": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-        "cli2": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-        "cli3": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-    }, 1),
-    # Fresh assignment with 2 replicas
-    ({
-        "cli1": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-        "cli2": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-        "cli3": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-        "cli4": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-    }, 2),
-    # Mimics adding a new client - No redundancy
-    ({
-        "cli1": CopartitionedAssignment(topics=_topics, standbys=set(),
-                                        actives={0, 2, 3, 4}),
-        "cli2": CopartitionedAssignment(topics=_topics, standbys=set(),
-                                        actives={1, 5, 6, 7}),
-        "cli3": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-    }, 0),
-    # Mimics adding a new client - With redundancy
-    ({
-        "cli1": CopartitionedAssignment(topics=_topics,
-                                        standbys={1, 5, 6, 7},
-                                        actives={0, 2, 3, 4}),
-        "cli2": CopartitionedAssignment(topics=_topics,
-                                        standbys={0, 2, 3, 4},
-                                        actives={1, 5, 6, 7}),
-        "cli3": CopartitionedAssignment(topics=_topics, actives=set(),
-                                        standbys=set()),
-    }, 1),
-    # Mimics removing a client - standbys available
-    ({
-        "cli1": CopartitionedAssignment(topics=_topics, actives={0, 1, 2},
-                                        standbys={3, 4, 7}),
-        "cli2": CopartitionedAssignment(topics=_topics, actives={3, 4, 5},
-                                        standbys={0, 2, 6}),
-    }, 1),
-])
-def test_get_assignment(client_assignments, replicas):
-    assignment = CopartitionedAssignor(
+def is_valid(cli_assignments: MutableMapping[str, CopartitionedAssignment],
+             num_partitions: int, replicas: int) -> bool:
+    all_partitions = set(range(num_partitions))
+    active_counts = Counter(partition
+                            for assignment in cli_assignments.values()
+                            for partition in assignment.actives)
+    assert all(
+        count == 1 for count in active_counts.values()
+    ), "Multiple clients assigned to same active"
+    assert set(active_counts.keys()) == all_partitions
+    standby_counts = Counter(partition
+                             for assignment in cli_assignments.values()
+                             for partition in assignment.standbys)
+    assert all(
+        count == replicas for count in standby_counts.values()
+    ), "Multiple clients assigned to same active"
+    assert not replicas or set(standby_counts.keys()) == all_partitions
+    return True
+
+
+def client_addition_sticky(old: MutableMapping[str, CopartitionedAssignment],
+                           new: MutableMapping[str, CopartitionedAssignment]
+                           ) -> bool:
+    assert all(
+        partition in old[client].actives
+        for client in old
+        for partition in new[client].actives
+    )
+    return True
+
+
+def client_removal_sticky(old: MutableMapping[str, CopartitionedAssignment],
+                          new: MutableMapping[str, CopartitionedAssignment]
+                          ) -> bool:
+    removed = set(old).difference(set(new))
+    reassigned_partitions = {partition
+                             for client, assignment in old.items()
+                             for partition in assignment.actives
+                             if client in removed}
+    assert all(
+        partition in old[client].actives or partition in reassigned_partitions
+        for client in new
+        for partition in new[client].actives
+    )
+    return True
+
+
+@given(partitions=integers(min_value=0, max_value=256),
+       replicas=integers(min_value=0, max_value=64),
+       num_clients=integers(min_value=1, max_value=1024))
+def test_fresh_assignment(partitions, replicas, num_clients):
+    assume(replicas < num_clients)
+    client_assignments = {
+        str(client): CopartitionedAssignment(topics=_topics)
+        for client in range(num_clients)
+    }
+    new_assignments = CopartitionedAssignor(
         _topics, client_assignments,
-        _num_partitions, replicas=replicas
+        partitions, replicas=replicas).get_assignment()
+    assert is_valid(new_assignments, partitions, replicas)
+
+
+@given(partitions=integers(min_value=0, max_value=256),
+       replicas=integers(min_value=0, max_value=64),
+       num_clients=integers(min_value=1, max_value=1024),
+       num_additional_clients=integers(min_value=1, max_value=16))
+def test_add_new_clients(partitions, replicas, num_clients,
+                         num_additional_clients):
+    assume(replicas < num_clients and num_additional_clients < num_clients)
+    client_assignments = {
+        str(client): CopartitionedAssignment(topics=_topics)
+        for client in range(num_clients)
+    }
+    valid_assignment = CopartitionedAssignor(
+        _topics, client_assignments,
+        partitions, replicas=replicas).get_assignment()
+    old_assignments = copy.deepcopy(valid_assignment)
+
+    # adding fresh clients
+    for client in range(num_clients, num_clients + num_additional_clients):
+        valid_assignment[str(client)] = CopartitionedAssignment(topics=_topics)
+
+    new_assignments = CopartitionedAssignor(
+        _topics, valid_assignment, partitions, replicas=replicas
     ).get_assignment()
-    assert False, f"Assignment: {assignment}"
+
+    assert is_valid(new_assignments, partitions, replicas)
+    assert client_addition_sticky(old_assignments, new_assignments)
+
+
+@given(partitions=integers(min_value=0, max_value=256),
+       replicas=integers(min_value=0, max_value=64),
+       num_clients=integers(min_value=1, max_value=1024),
+       num_removal_clients=integers(min_value=1, max_value=16))
+def test_remove_clients(partitions, replicas, num_clients,
+                        num_removal_clients):
+    assume(num_removal_clients < num_clients and
+           replicas < (num_clients - num_removal_clients))
+    client_assignments = {
+        str(client): CopartitionedAssignment(topics=_topics)
+        for client in range(num_clients)
+    }
+    valid_assignment = CopartitionedAssignor(
+        _topics, client_assignments,
+        partitions, replicas=replicas).get_assignment()
+    old_assignments = copy.deepcopy(valid_assignment)
+
+    # adding fresh clients
+    for client in range(num_clients - num_removal_clients, num_clients):
+        del valid_assignment[str(client)]
+
+    new_assignments = CopartitionedAssignor(
+        _topics, valid_assignment, partitions, replicas=replicas
+    ).get_assignment()
+
+    assert is_valid(new_assignments, partitions, replicas)
+    assert client_removal_sticky(old_assignments, new_assignments)
