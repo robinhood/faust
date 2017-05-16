51757c8c0fb3d2fef7f9f02f8cf762ecf2c082ea
Vineet Goel <vineet@robinhood.com>
2017-05-16 11:17:31 -0700

    typecheck, add .mypy_cache/ to gitignore

--- faust/transport/kafka/protocol/api.py
+++ faust/transport/kafka/protocol/api.py
@@ -3,32 +3,33 @@ from __future__ import absolute_import
 import abc
 
 from kafka.protocol.struct import Struct
+from kafka.protocol.types import Schema
 
 
 class Request(Struct):
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractproperty
-    def API_KEY(self):
+    def API_KEY(self) -> int:
         """Integer identifier for api request"""
         pass
 
     @abc.abstractproperty
-    def API_VERSION(self):
+    def API_VERSION(self) -> int:
         """Integer of api request version"""
         pass
 
     @abc.abstractproperty
-    def SCHEMA(self):
+    def SCHEMA(self) -> Schema:
         """An instance of Schema() representing the request structure"""
         pass
 
     @abc.abstractproperty
-    def RESPONSE_TYPE(self):
+    def RESPONSE_TYPE(self) -> Response:
         """The Response class associated with the api request"""
         pass
 
-    def expect_response(self):
+    def expect_response(self) -> bool:
         """Override this method if an api request does not always
         generate a response"""
         return True
@@ -38,16 +39,16 @@ class Response(Struct):
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractproperty
-    def API_KEY(self):
+    def API_KEY(self) -> int:
         """Integer identifier for api request/response"""
         pass
 
     @abc.abstractproperty
-    def API_VERSION(self):
+    def API_VERSION(self) -> int:
         """Integer of api request/response version"""
         pass
 
     @abc.abstractproperty
-    def SCHEMA(self):
+    def SCHEMA(self) -> Schema:
         """An instance of Schema() representing the response structure"""
         pass
