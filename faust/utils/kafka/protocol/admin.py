7ea44e71c6671c814e566ff2c5722be36ecb2f92
Vineet Goel <vineet@robinhood.com>
2017-05-19 00:01:25 -0700

    Using aiokafka client. Can configure retention period in addition to changelogging

--- faust/utils/kafka/protocol/admin.py
+++ faust/utils/kafka/protocol/admin.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 
-from .api import Request, Response
 from kafka.protocol.types import (
     Array, Boolean, Int16, Int32, Schema, String,
 )
+
+from ..protocol.api import Request, Response
 
 
 class CreateTopicsResponse_v0(Response):
