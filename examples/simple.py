3bb97b1e80d3046d108b6b9634a4f1349f97dc2e
Ask Solem <ask@celeryproject.org>
2017-05-26 15:25:29 -0700

    examples/simple: Dont set window directly

--- examples/simple.py
+++ examples/simple.py
@@ -20,8 +20,8 @@ withdrawals_topic = app.topic('withdrawals', value_type=Withdrawal)
 @app.actor(withdrawals_topic)
 async def find_large_withdrawals(withdrawals):
     user_to_total = app.table('user_to_total', default=int)
-    country_to_total = app.table('country_to_total', default=int,
-                                 window=faust.TumblingWindow(10.0))
+    country_to_total = app.table(
+        'country_to_total', default=int).tumbling(10.0)
     async for withdrawal in withdrawals:
         user_to_total[withdrawal.user] += withdrawal.amount
         country_to_total[withdrawal.country] += withdrawal.amount
