16dedcde4e4cca9962e4b02c5f5e76516a5e97ee
Vineet Goel <vineet@robinhood.com>
2017-08-24 19:06:32 -0700

    Add assignment endpoint to see client assignments

--- faust/web/apps/stats/app.py
+++ faust/web/apps/stats/app.py
@@ -1,7 +1,13 @@
+from collections import defaultdict
+from typing import Iterable, List, MutableMapping
+from faust.types.topics import TopicPartition
 from faust.web import views
 from faust.web.base import Request, Response, Web
 
-__all__ = ['Stats', 'Site']
+__all__ = ['Assignment', 'Stats', 'Site']
+
+
+TPMap = MutableMapping[str, List[int]]
 
 
 class Stats(views.View):
@@ -14,7 +20,28 @@ class Stats(views.View):
         })
 
 
+class Assignment(views.View):
+    package = 'faust.web.apps.stats'
+
+    @classmethod
+    def _topic_grouped(
+            cls,
+            assignment: Iterable[TopicPartition]) -> TPMap:
+        tps = defaultdict(list)
+        for tp in assignment:
+            tps[tp.topic].append(tp.partition)
+        return dict(tps)
+
+    async def get(self, web: Web, request: Request) -> Response:
+        assignor = self.app.assignor
+        return web.json({
+            'actives': self._topic_grouped(assignor.assigned_actives()),
+            'standbys': self._topic_grouped(assignor.assigned_standbys()),
+        })
+
+
 class Site(views.Site):
     views = {
         '/': Stats,
+        '/assignment': Assignment,
     }
