cef134782cf0fe4b6ba7fb0bb3d9b68685504bbf
Ask Solem <ask@celeryproject.org>
2017-09-26 16:31:00 -0700

    Reorg: faust.actors is now a package

--- faust/actors/actor.py
+++ faust/actors/actor.py
@@ -1,33 +1,32 @@
 import asyncio
 import typing
 
-from collections import defaultdict
 from typing import (
-    Any, AsyncIterable, AsyncIterator, Awaitable,
-    Callable, Iterable, List, MutableMapping,
-    MutableSequence, Set, Tuple, Type, Union, cast,
+    Any, AsyncIterable, AsyncIterator, Awaitable, Callable,
+    Iterable, List, MutableMapping, MutableSequence, Tuple, Type, Union, cast,
 )
 from uuid import uuid4
-from weakref import WeakSet
 
-from . import Record
-from .types import (
-    AppT, ChannelT, CodecArg, K, MessageSentCallback, ModelT,
+from .models import ReqRepRequest, ReqRepResponse
+from .replies import BarrierState, ReplyPromise
+
+from ..types import (
+    AppT, ChannelT, CodecArg, K, MessageSentCallback,
     RecordMetadata, StreamT, TopicT, V,
 )
-from .types.actors import (
+from ..types.actors import (
     ActorErrorHandler, ActorFun, ActorInstanceT, ActorRefT, ActorT,
     AsyncIterableActorT, AwaitableActorT, ReplyToArg, SinkT, _T,
 )
-from .utils.aiter import aenumerate, aiter
-from .utils.collections import NodeT
-from .utils.futures import maybe_async
-from .utils.logging import get_logger
-from .utils.objects import cached_property, canoname, qualname
-from .utils.services import (
+from ..utils.aiter import aenumerate, aiter
+from ..utils.collections import NodeT
+from ..utils.futures import maybe_async
+from ..utils.logging import get_logger
+from ..utils.objects import cached_property, canoname, qualname
+from ..utils.services import (
     OneForOneSupervisor, Service, ServiceT, SupervisorStrategyT,
 )
-from .utils.services.proxy import ServiceProxy
+from ..utils.services.proxy import ServiceProxy
 
 if typing.TYPE_CHECKING:
     from .app import App
@@ -35,14 +34,9 @@ else:
     class App: ...   # noqa
 
 __all__ = [
-    'ReqRepRequest',
-    'ReqRepResponse',
-    'ReplyPromise',
-    'ReplyConsumer',
     'ActorInstance',
     'AsyncIterableActor',
     'AwaitableActor',
-    'ActorService',
     'Actor',
 ]
 
@@ -104,159 +98,6 @@ logger = get_logger(__name__)
 #
 # TIP: Sinks can also be added as an argument to the ``@actor`` decorator:
 #      ``@app.actor(sinks
-
-
-# XXX "namespace" below is the Avro registry key.
-# It's going to be included in every single actor request,
-# so I figured "org.faust.ReqRepRequest" was too long,
-# but maybe that's silly?
-class ReqRepRequest(Record, serializer='json', namespace='@RRReq'):
-    """Value wrapped in a Request-Reply request."""
-
-    # actor.ask(value) wraps the value in this record
-    # so that the receiving actor knows where to send the reply.
-
-    value: ModelT
-    reply_to: str
-    correlation_id: str
-
-
-class ReqRepResponse(Record, serializer='json', namespace='@RRRes'):
-    """Request-Reply response."""
-
-    key: K
-    value: ModelT
-    correlation_id: str
-
-
-class ReplyPromise(asyncio.Future):
-    """Reply promise can be awaited to wait until result ready."""
-    reply_to: str
-    correlation_id: str
-
-    def __init__(self, reply_to: str, correlation_id: str,
-                 **kwargs: Any) -> None:
-        self.reply_to = reply_to
-        self.correlation_id = correlation_id
-        super().__init__(**kwargs)
-
-    def fulfill(self, correlation_id: str, value: Any) -> None:
-        # If it wasn't for BarrierState we would just use .set_result()
-        # directly, but BarrierState.fulfill requires the correlation_id
-        # to be sent with it. That way it can mark that part of the map
-        # operation as completed.
-        assert correlation_id == self.correlation_id
-        self.set_result(value)
-
-
-class BarrierState(ReplyPromise):
-    #: This is the size while the messages are being sent.
-    #: (it's a tentative total, added to until the total is finalized).
-    size: int = 0
-
-    #: This is the actual total when all messages have been sent.
-    #: It's set by :meth:`finalize`.
-    total: int = 0
-
-    #: The number of results we have received.
-    fulfilled: int = 0
-
-    #: Internal queue where results are added to.
-    _results: asyncio.Queue
-
-    #: Set of pending replies that this barrier is composed of.
-    pending: Set[ReplyPromise]
-
-    def __init__(self, reply_to: str, **kwargs: Any) -> None:
-        super().__init__(reply_to=reply_to, correlation_id=None, **kwargs)
-        self.pending = set()
-        loop: asyncio.AbstractEventLoop = self._loop  # type: ignore
-        self._results = asyncio.Queue(maxsize=1000, loop=loop)
-
-    def add(self, p: ReplyPromise) -> None:
-        self.pending.add(p)
-        self.size += 1
-
-    def finalize(self) -> None:
-        self.total = self.size
-        # The barrier may have been filled up already at this point,
-        if self.fulfilled >= self.total:
-            self.set_result(True)
-
-    def fulfill(self, correlation_id: str, value: Any) -> None:
-        # ReplyConsumer calls this whenever a new reply is received.
-        self._results.put_nowait((correlation_id, value))
-        self.fulfilled += 1
-        if self.total:
-            if self.fulfilled >= self.total:
-                self.set_result(True)
-
-    def get_nowait(self) -> Tuple[str, Any]:
-        """Return next reply, or raise :exc:`asyncio.QueueEmpty`."""
-        return self._results.get_nowait()
-
-    async def iterate(self) -> AsyncIterator[Tuple[str, Any]]:
-        """Iterate over results as arrive."""
-        get = self._results.get
-        get_nowait = self._results.get_nowait
-        is_done = self.done
-        while not is_done():
-            yield await get()
-        while 1:
-            try:
-                yield get_nowait()
-            except asyncio.QueueEmpty:
-                break
-
-
-class ReplyConsumer(Service):
-    if typing.TYPE_CHECKING:
-        _waiting: MutableMapping[str, WeakSet[ReplyPromise]]
-    _waiting = None
-    _fetchers: MutableMapping[str, asyncio.Future]
-
-    def __init__(self, app: AppT, **kwargs: Any) -> None:
-        self.app = app
-        self._waiting = defaultdict(WeakSet)
-        self._fetchers = {}
-        super().__init__(**kwargs)
-
-    async def on_start(self) -> None:
-        if self.app.create_reply_topic:
-            await self._start_fetcher(self.app.reply_to)
-
-    async def add(self, correlation_id: str, promise: ReplyPromise) -> None:
-        reply_topic = promise.reply_to
-        if reply_topic not in self._fetchers:
-            await self._start_fetcher(reply_topic)
-        self._waiting[correlation_id].add(promise)
-
-    async def _start_fetcher(self, topic_name: str) -> None:
-        if topic_name not in self._fetchers:
-            # set the key as a lock, so it doesn't happen twice
-            self._fetchers[topic_name] = None
-            # declare the topic
-            topic = self._reply_topic(topic_name)
-            await topic.maybe_declare()
-            await self.sleep(3.0)
-            # then create the future
-            self._fetchers[topic_name] = self.add_future(
-                self._drain_replies(topic))
-
-    async def _drain_replies(self, channel: ChannelT):
-        async for reply in channel.stream():
-            for promise in self._waiting[reply.correlation_id]:
-                promise.fulfill(reply.correlation_id, reply.value)
-
-    def _reply_topic(self, topic: str) -> TopicT:
-        return self.app.topic(
-            topic,
-            partitions=1,
-            replicas=0,
-            deleting=True,
-            retention=self.app.reply_expires,
-            value_type=ReqRepResponse,
-        )
 
 
 class ActorInstance(ActorInstanceT, Service):
@@ -710,3 +551,6 @@ class Actor(ActorT, ServiceProxy):
     @property
     def label(self) -> str:
         return f'{type(self).__name__}: {qualname(self.fun)}'
+
+
+__flake8_MutableMapping_is_used: MutableMapping  # XXX flake8 bug
