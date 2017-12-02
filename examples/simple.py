b153731b771ed790e4101e9e58d08fff4419338e
Ask Solem <ask@celeryproject.org>
2017-12-01 16:30:26 -0800

    Cleanup simple example

--- examples/simple.py
+++ examples/simple.py
@@ -19,7 +19,6 @@ import os
 import random
 from datetime import datetime, timezone
 from itertools import count
-from time import monotonic
 import faust
 from faust.cli import option
 
@@ -47,55 +46,19 @@ country_to_total = app.Table(
     'country_to_total', default=int).tumbling(10.0, expires=10.0)
 
 
-@app.agent()
-async def _objdump(requests):
-    import objgraph
-    async for request in requests:
-        objgraph.show_most_common_types(limit=50)
-
-
-
-graph_topic = app.topic('examples.simple._graph', value_type=str,
-        value_serializer='raw')
-@app.agent(graph_topic)
-async def _graph(requests):
-    import objgraph
-    import random
-    async for typ_ in requests:
-        obj = random.choice(objgraph.by_type(typ_))
-        objgraph.show_backrefs(obj, max_depth=10)
-
-
-@app.command()
-async def objdump():
-    """dump common object types to worker log."""
-    await _objdump.send(1)
-
-
-@app.command()
-async def graph():
-    await _graph.send(1)
-
-
 @app.agent(withdrawals_topic)
 async def find_large_user_withdrawals(withdrawals):
-    async for withdrawal in withdrawals.group_by(Withdrawal.user):
+    async for withdrawal in withdrawals:
         #print(f'Withdrawal: {withdrawal}')
         ...
         #user_to_total[withdrawal.user] += withdrawal.amount
-
-
-@user_to_total.on_recover
-async def echo_size():
-    await asyncio.sleep(1.0)
-    print('Total size', len(user_to_total))
 
 
 @app.command(
         option('--max-latency',
                type=float, default=PRODUCE_LATENCY,
                help='Sleep (at most) n seconds between publishing.'))
-async def produce(self, max_latency: float):
+async def produce(self, stop: int, max_latency: float):
     """Produce example Withdrawal events."""
     num_countries = 5
     countries = [f'country_{i}' for i in range(num_countries)]
@@ -103,7 +66,7 @@ async def produce(self, max_latency: float):
     num_users = 500
     users = [f'user_{i}' for i in range(num_users)]
     self.say('Done setting up. SENDING!')
-    for i in count():
+    for i in range(stop) if stop is not None else count():
         withdrawal = Withdrawal(
             user=random.choice(users),
             amount=random.uniform(0, 25_000),
