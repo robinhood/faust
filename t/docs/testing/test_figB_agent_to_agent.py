c9cabcefdc6abf924a2226a54d5d47f5b5d0e642
Ask Solem <ask@celeryproject.org>
2018-01-22 13:12:25 -0800

    Adds tests for testing gudie examples

---
+++ t/docs/testing/test_figB_agent_to_agent.py
@@ -0,0 +1,47 @@
+from unittest.mock import Mock, patch
+import faust
+import pytest
+
+app = faust.App('example-test-agent-call')
+
+
+@app.agent()
+async def foo(stream):
+    async for value in stream:
+        await bar.send(value)
+        yield value
+
+
+@app.agent()
+async def bar(stream):
+    async for value in stream:
+        yield value + 'YOLO'
+
+
+@pytest.fixture()
+def test_app():
+    app.store = 'memory://'
+    return app
+
+
+def mock_coro(return_value=None, **kwargs):
+    """Create mock coroutine function."""
+    async def wrapped(*args, **kwargs):
+        return return_value
+    return Mock(wraps=wrapped, **kwargs)
+
+
+@pytest.mark.asyncio()
+async def test_foo(test_app):
+    with patch(__name__ + '.bar') as mocked_bar:
+        mocked_bar.send = mock_coro()
+        async with foo.test_context() as agent:
+            await agent.put('hey')
+            mocked_bar.send.assert_called_with('hey')
+
+
+@pytest.mark.asyncio()
+async def test_bar(test_app):
+    async with bar.test_context() as agent:
+        event = await agent.put('hey')
+        assert agent.results[event.message.offset] == 'heyYOLO'
---
+++ t/docs/testing/test_figC_windowed_table.py
@@ -0,0 +1,39 @@
+import faust
+import pytest
+
+
+class Order(faust.Record, serializer='json'):
+    account_id: str
+    product_id: str
+    amount: int
+    price: float
+
+
+app = faust.App('test-example')
+orders_topic = app.topic('orders', value_type=Order)
+
+# order count within the last hour (window is a 1-hour TumblingWindow).
+orders_for_account = app.Table(
+    'order-count-by-account', default=int,
+).tumbling(3600).relative_to_stream()
+
+
+@app.agent(orders_topic)
+async def process_order(orders):
+    async for order in orders.group_by(Order.account_id):
+        orders_for_account[order.account_id] += 1
+        yield order
+
+
+@pytest.mark.asyncio()
+async def test_process_order():
+    app.store = 'memory://'
+    async with process_order.test_context() as agent:
+        order = Order(account_id='1', product_id='2', amount=1, price=300)
+        event = await agent.put(order)
+
+        # windowed table: we select window relative to the current event
+        assert orders_for_account['1'].current(event) == 1
+
+        # in the window 3 hours ago there were no orders:
+        assert not orders_for_account['1'].delta(3600 * 3, event)
