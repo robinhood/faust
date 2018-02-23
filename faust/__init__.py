730b32d2acdde65d7f543828ee80e2af95b3d8ae
Ask Solem <ask@celeryproject.org>
2018-02-22 23:30:01 -0800

    Bump version: 0.9.26 → 0.9.27

--- faust/__init__.py
+++ faust/__init__.py
@@ -23,7 +23,7 @@ import sys
 import typing
 from typing import Any, Mapping, NamedTuple, Optional, Sequence, Tuple
 
-__version__ = '0.9.26'
+__version__ = '0.9.27'
 __author__ = 'Robinhood Markets'
 __contact__ = 'opensource@robinhood.com'
 __homepage__ = 'http://fauststream.com'
