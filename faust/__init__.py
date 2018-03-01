9214282c0088b0683529c6a3e89dd258852914f9
Vineet Goel <vineet@robinhood.com>
2018-02-28 20:56:38 -0800

    Bump version: 0.9.33 → 0.9.34

--- faust/__init__.py
+++ faust/__init__.py
@@ -23,7 +23,7 @@ import sys
 import typing
 from typing import Any, Mapping, NamedTuple, Optional, Sequence, Tuple
 
-__version__ = '0.9.33'
+__version__ = '0.9.34'
 __author__ = 'Robinhood Markets'
 __contact__ = 'opensource@robinhood.com'
 __homepage__ = 'http://fauststream.com'
