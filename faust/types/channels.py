be3b6ac8e9bc0e522fba09901501765b1b521f85
Ask Solem <ask@celeryproject.org>
2018-03-15 16:21:09 -0700

    Accidentally left EventT in types.channels after move to types.events

--- faust/types/channels.py
+++ faust/types/channels.py
@@ -13,57 +13,17 @@ from .tuples import FutureMessage, Message, MessageSentCallback, RecordMetadata
 
 if typing.TYPE_CHECKING:
     from .app import AppT
+    from .events import EventT
     from .models import ModelArg
     from .streams import StreamT
     from .transports import ConsumerT, TPorTopicSet
 else:
     class AppT: ...             # noqa
+    class EventT: ...           # noqa
     class ModelArg: ...         # noqa
     class StreamT: ...          # noqa
     class ConsumerT: ...        # noqa
     class TPorTopicSet: ...     # noqa
-
-
-class EventT(AsyncContextManager):
-    app: AppT
-    key: K
-    value: V
-    message: Message
-    acked: bool
-
-    __slots__ = ('app', 'key', 'value', 'message', 'acked')
-
-    @abc.abstractmethod
-    def __init__(self, app: AppT, key: K, value: V, message: Message) -> None:
-        ...
-
-    @abc.abstractmethod
-    async def send(self,
-                   channel: Union[str, 'ChannelT'],
-                   key: K = None,
-                   value: V = None,
-                   partition: int = None,
-                   key_serializer: CodecArg = None,
-                   value_serializer: CodecArg = None,
-                   callback: MessageSentCallback = None,
-                   force: bool = False) -> Awaitable[RecordMetadata]:
-        ...
-
-    @abc.abstractmethod
-    async def forward(self,
-                      channel: Union[str, 'ChannelT'],
-                      key: Any = None,
-                      value: Any = None,
-                      partition: int = None,
-                      key_serializer: CodecArg = None,
-                      value_serializer: CodecArg = None,
-                      callback: MessageSentCallback = None,
-                      force: bool = False) -> Awaitable[RecordMetadata]:
-        ...
-
-    @abc.abstractmethod
-    def ack(self) -> bool:
-        ...
 
 
 class ChannelT(AsyncIterator):
