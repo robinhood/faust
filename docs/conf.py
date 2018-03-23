4b1bfdc77ab93c9e42e34e8ae31b685e3340a03a
Ask Solem <ask@celeryproject.org>
2018-03-22 17:16:28 -0700

    Docs: Adds intersphinx mapping for MyPy

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ globals().update(conf.build_config(
         'aiokafka': ('https://aiokafka.readthedocs.io/en/stable/', None),
         'click': ('http://click.pocoo.org/6/', None),
         'mode': ('https://mode.readthedocs.io/en/latest/', None),
+        'mypy': ('https://mypy.readthedocs.io/en/latest/', None),
     },
     # django_settings='testproj.settings',
     # from pathlib import Path
