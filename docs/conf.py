037588ec354fbd79ee4d2208c668e212b52d8b24
Ask Solem <ask@celeryproject.org>
2018-03-22 17:17:04 -0700

    Docs: Adds intersphinx mapping for terminaltables

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ globals().update(conf.build_config(
         'mode': ('https://mode.readthedocs.io/en/latest/', None),
         'mypy': ('https://mypy.readthedocs.io/en/latest/', None),
         'rocksdb': ('http://python-rocksdb.readthedocs.io/en/latest/', None),
+        'terminaltables': ('https://robpol86.github.io/terminaltables/', None),
     },
     # django_settings='testproj.settings',
     # from pathlib import Path
