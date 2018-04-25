85c3a4f952d518226fdff43032c57586a9732ef2
Ask Solem <ask@celeryproject.org>
2018-04-25 15:29:09 -0700

    Remove consumer flight recorder

--- faust/transport/consumer.py
+++ faust/transport/consumer.py
@@ -426,35 +426,24 @@ class Consumer(Service, ConsumerT):
 
         try:
             while not (consumer_should_stop() or fetcher_should_stop()):
-                with flight_recorder(self.log, timeout=30.0) as on_timeout:
-                    set_flag(flag_consumer_fetching)
-                    on_timeout.info('+getmany(timeout=5.0)')
-                    ait = cast(AsyncIterator, getmany(timeout=5.0))
-                    on_timeout.info('-getmany(timeout=5.0)')
-                    # Sleeping because sometimes getmany is called in a loop
-                    # never releasing to the event loop
-                    on_timeout.info('+sleep(0)')
-                    await self.sleep(0)
-                    on_timeout.info('-sleep(0)')
-                    on_timeout.info('+iterate over fetch result')
-                    async for tp, message in ait:
-                        offset = message.offset
-                        r_offset = get_read_offset(tp)
-                        if r_offset is None or offset > r_offset:
-                            if commit_every is not None:
-                                if self._n_acked >= commit_every:
-                                    self._n_acked = 0
-                                    on_timeout.info('+commit() every')
-                                    await self.commit()
-                                    on_timeout.info('-commit() every')
-                            on_timeout.info('+consumer callback()')
-                            await callback(message)
-                            on_timeout.info('-consumer callback()')
-                            set_read_offset(tp, offset)
-                        else:
-                            self.log.dev('DROPPED MESSAGE ROFF %r: k=%r v=%r',
-                                         offset, message.key, message.value)
-                    on_timeout.info('-iterate over fetch result')
+                set_flag(flag_consumer_fetching)
+                ait = cast(AsyncIterator, getmany(timeout=5.0))
+                # Sleeping because sometimes getmany is called in a loop
+                # never releasing to the event loop
+                await self.sleep(0)
+                async for tp, message in ait:
+                    offset = message.offset
+                    r_offset = get_read_offset(tp)
+                    if r_offset is None or offset > r_offset:
+                        if commit_every is not None:
+                            if self._n_acked >= commit_every:
+                                self._n_acked = 0
+                                await self.commit()
+                        await callback(message)
+                        set_read_offset(tp, offset)
+                    else:
+                        self.log.dev('DROPPED MESSAGE ROFF %r: k=%r v=%r',
+                                     offset, message.key, message.value)
                 unset_flag(flag_consumer_fetching)
 
         except self.consumer_stopped_errors:
