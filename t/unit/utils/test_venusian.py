68ef83f66b9b4b030b039b0045fb11507128cdd4
Ask Solem <ask@celeryproject.org>
2018-05-04 09:48:40 -0700

    100% coverage for .utils.venusian

---
+++ t/unit/utils/test_venusian.py
@@ -0,0 +1,16 @@
+from unittest.mock import Mock, patch
+import pytest
+from faust.utils import venusian
+
+
+def test_attach():
+    callable = Mock(name='callable')
+    category = 'category'
+    with patch('faust.utils.venusian._attach') as _attach:
+        with patch('faust.utils.venusian._on_found') as _on_found:
+            venusian.attach(callable, category)
+            _attach.assert_called_with(callable, _on_found, category=category)
+
+
+def test_on_found():
+    venusian._on_found(Mock(name='scanner'), 'name', Mock(name='obj'))
