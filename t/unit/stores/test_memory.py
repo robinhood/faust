5afa899c69f488421fb30fe5deed30e03816589a
Ask Solem <ask@celeryproject.org>
2018-05-05 19:43:52 -0700

    100% coverage for faust.stores.memory

---
+++ t/unit/stores/test_memory.py
@@ -0,0 +1,35 @@
+from unittest.mock import Mock
+import pytest
+from faust.stores.memory import Store
+from faust.types import TP
+
+
+class test_Store:
+
+    @pytest.fixture
+    def store(self, *, app):
+        return Store(url='memory://', app=app)
+
+    def test_clear(self, *, store):
+        store.data['foo'] = 1
+        store._clear()
+        assert not store.data
+
+    def test_apply_changelog_batch(self, *, store):
+        event1 = Mock(name='event1')
+        event1.key = b'key'
+        event1.value = b'value'
+        events = [event1]
+        to_key = Mock(name='to_key')
+        to_value = Mock(name='to_value')
+        store.apply_changelog_batch(events, to_key=to_key, to_value=to_value)
+
+        to_key.assert_called_once_with(b'key')
+        to_value.assert_called_once_with(b'value')
+        assert store.data[to_key()] is to_value()
+
+    def test_persisted_offset(self, *, store):
+        assert store.persisted_offset(TP('foo', 0)) is None
+
+    def test_reset_state(self, *, store):
+        store.reset_state()
