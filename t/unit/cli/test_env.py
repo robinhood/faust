b677fa223fb2ac894c52a244a61efa19ca59c1fc
Ask Solem <ask@celeryproject.org>
2018-05-07 11:08:07 -0700

    100% coverage for fausts.cli._env

---
+++ t/unit/cli/test_env.py
@@ -0,0 +1,10 @@
+import os
+from unittest.mock import patch
+import pytest
+from faust.cli._env import _getenv
+
+
+def test_getenv_not_set():
+    os.environ.pop('THIS_IS_NOT_SET', None)
+    with pytest.raises(KeyError):
+        _getenv('THIS_IS_NOT_SET')
