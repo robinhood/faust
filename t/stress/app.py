f54ceac8fb7cb779ed9b43bccec8a575ccfe6a66
Ask Solem <ask@celeryproject.org>
2018-07-03 14:07:00 -0700

    [stress] Better progress reporting

--- t/stress/app.py
+++ t/stress/app.py
@@ -11,10 +11,12 @@ ProducerFun = Callable[[int], Iterator[RecordMetadata]]
 
 class StressApp(faust.App):
     stress_producers: List[ProducerFun]
+    count_received_events: int
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stress_producers = []
+        self.count_received_events = 0
 
     def register_stress_producer(self, fun: ProducerFun):
         self.stress_producers.append(fun)
@@ -32,4 +34,25 @@ def create_stress_app(name, origin, **kwargs: Any) -> StressApp:
         autodiscover=True,
         **kwargs)
     producer.install_produce_command(app)
+
+    @app.task
+    async def report_progress(app):
+        prev_count = 0
+        times_failed = 0
+        while not app.should_stop:
+            severity = app.log.info
+            await app._service.sleep(5.0)
+            if app.count_received_events <= prev_count:
+                times_failed += 1
+                if times_failed > 6:
+                    severity = app.log.error
+                elif times_failed > 3:
+                    severity = app.log.warn
+                severity(f'{app.conf.id} not progressing: '
+                         f'was {prev_count} now {app.count_received_events}')
+            else:
+                times_failed = 0
+                severity(f'{app.conf.id} progressing: '
+                         f'was {prev_count} now {app.count_received_events}')
+            prev_count = app.count_received_events
     return app
