229239c207dd106e6d4a40ed62d9ae3321ec254a
Ask Solem <ask@celeryproject.org>
2018-07-03 16:13:14 -0700

    [stress] Forgot to commit apps

--- t/stress/tests/forwarder/app.py
+++ t/stress/tests/forwarder/app.py
@@ -10,8 +10,6 @@ app = create_stress_app(
     broker_commit_every=100,
 )
 
-counter_received = 0
-
 
 @app.task
 async def on_leader_send_monotonic_counter(app, max_latency=0.01) -> None:
@@ -38,7 +36,6 @@ async def forward(numbers: Stream[int]) -> None:
 async def receive(forwarded_numbers: Stream[int]) -> None:
     # last agent recveices number and verifies numbers are always increasing.
     # (repeating or decreasing numbers are evidence of duplicates).
-    global counter_received
     previous_number = None
     async for number in forwarded_numbers:
         assert isinstance(number, int)
@@ -46,18 +43,4 @@ async def receive(forwarded_numbers: Stream[int]) -> None:
             if number > 0:
                 # consider 0 as the service being restarted.
                 assert number > previous_number
-        counter_received += 1
-
-
-@app.task
-async def report_progress(app):
-    prev_count = 0
-    while not app.should_stop:
-        await app._service.sleep(5.0)
-        if counter_received <= prev_count:
-            print(f'Forwarder not progressing: '
-                  f'was {prev_count} now {counter_received}')
-        else:
-            print(f'Forwarder progressing: '
-                  f'was {prev_count} now {counter_received}')
-        prev_count = counter_received
+        app.count_received_events += 1
--- t/stress/tests/simple/app.py
+++ t/stress/tests/simple/app.py
@@ -7,7 +7,6 @@ app = create_stress_app(
 )
 
 withdrawals_topic = app.topic('f-stress-withdrawals', value_type=Withdrawal)
-seen_events = 0
 
 
 @app.register_stress_producer
@@ -18,21 +17,5 @@ async def produce_withdrawals(max_messages):
 
 @app.agent(withdrawals_topic)
 async def track_user_withdrawal(withdrawals):
-    global seen_events
-    i = 0
     async for _withdrawal in withdrawals:  # noqa
-        i += 1
-        seen_events += 1
-
-
-@app.task
-async def report_progress(app):
-    prev_count = 0
-    while not app.should_stop:
-        await app._service.sleep(5.0)
-        if seen_events <= prev_count:
-            print(f'Simple not progressing: '
-                  f'was {prev_count} now {seen_events}')
-        else:
-            print(f'Simple Progressing: was {prev_count} now {seen_events}')
-        prev_count = seen_events
+        app.count_received_events += 1
