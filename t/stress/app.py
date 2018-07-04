28f14ab6ee2c6350c60ed436e2f729d7383635fd
Ask Solem <ask@celeryproject.org>
2018-07-04 13:49:11 -0700

    [stress] Views and test status command

--- t/stress/app.py
+++ t/stress/app.py
@@ -1,8 +1,13 @@
 from typing import Any, Callable, Iterator, List
 import faust
 from faust.types import RecordMetadata
+from faust.web import Request, Response, Web
 from . import config
 from . import producer
+
+STATUS_OK = 'OK'
+STATUS_SLOW = 'SLOW'
+STATUS_STALL = 'STALL'
 
 __all__ = ['ProducerFun', 'StressApp', 'create_stress_app']
 
@@ -13,6 +18,10 @@ class StressApp(faust.App):
     stress_producers: List[ProducerFun]
     count_received_events: int
 
+    #: Status page reports this number.
+    #: and report_progress background thread updates it as tests fail.
+    faults: int = 0
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stress_producers = []
@@ -21,6 +30,15 @@ class StressApp(faust.App):
     def register_stress_producer(self, fun: ProducerFun):
         self.stress_producers.append(fun)
         return fun
+
+
+def faults_to_human_status(app):
+    if app.faults > 6:
+        return STATUS_STALL
+    elif app.faults > 3:
+        return STATUS_SLOW
+    else:
+        return STATUS_OK
 
 
 def create_stress_app(name, origin, **kwargs: Any) -> StressApp:
@@ -38,21 +56,38 @@ def create_stress_app(name, origin, **kwargs: Any) -> StressApp:
     @app.task
     async def report_progress(app):
         prev_count = 0
-        times_failed = 0
         while not app.should_stop:
             severity = app.log.info
             await app._service.sleep(5.0)
             if app.count_received_events <= prev_count:
-                times_failed += 1
-                if times_failed > 6:
+                app.faults += 1
+                if app.faults > 6:
                     severity = app.log.error
-                elif times_failed > 3:
+                elif app.faults > 3:
                     severity = app.log.warn
-                severity(f'{app.conf.id} not progressing: '
+                severity(f'{app.conf.id} not progressing (x{app.faults}): '
                          f'was {prev_count} now {app.count_received_events}')
             else:
-                times_failed = 0
+                app.faults = 0
                 severity(f'{app.conf.id} progressing: '
                          f'was {prev_count} now {app.count_received_events}')
             prev_count = app.count_received_events
+
+    @app.page('/test/status/')
+    async def get_status(web: Web, request: Request) -> Response:
+        return web.json(
+            {'status': faults_to_human_status(app), 'faults': app.faults},
+        )
+
+    @app.command()
+    async def status(self):
+        async with app.http_client as client:
+            async with client.get('http://localhost:6066/test/status/') as r:
+                content = await r.json()
+                status = content['status']
+                if status == STATUS_OK:
+                    print(self.color('green', status))
+                else:
+                    print(self.color('red', status))
+
     return app
