aa2cc420d729326006f2968f382e484d51a73ed6
Ask Solem <ask@celeryproject.org>
2018-07-06 11:57:23 -0700

    [stress][forwarder] try different strategy

--- t/stress/tests/forwarder/app.py
+++ t/stress/tests/forwarder/app.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+from collections import Counter
 from faust import Stream
 from faust.sensors import checks
 from ...app import create_stress_app
@@ -7,7 +8,7 @@ from ...app import create_stress_app
 counter_received = 0
 
 app = create_stress_app(
-    name='f-stress-duplicates',
+    name='f-stress-duplication',
     origin='t.stress.tests.forwarder',
     stream_wait_empty=False,
     broker_commit_every=100,
@@ -20,16 +21,22 @@ app.add_system_check(
     ),
 )
 
+partitions = 100
+partitions_sent_counter = Counter()
+
 
 @app.task
-async def on_leader_send_monotonic_counter(app, max_latency=0.01) -> None:
+async def on_leader_send_monotonic_counter(app, max_latency=0.08) -> None:
     # Leader node sends incrementing numbers to a topic
-    counter_sent = 0
+
+    partitions_sent_counter.clear()
 
     while not app.should_stop:
         if app.is_leader():
-            await forward.send(value=counter_sent)
-            counter_sent += 1
+            for partition in range(partitions):
+                current_value = partitions_sent_counter.get(partition, 0)
+                await forward.send(value=current_value, partition=partition)
+                partitions_sent_counter[partition] += 1
             await asyncio.sleep(random.uniform(0, max_latency))
         else:
             await asyncio.sleep(1)
