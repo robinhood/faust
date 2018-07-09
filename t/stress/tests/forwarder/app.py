e29d7aab63cee48c9b8ef3bf8135254de07de6dc
Ask Solem <ask@celeryproject.org>
2018-07-09 15:13:35 -0700

    [stress] Report on gaps in sequence (Issue #115)

--- t/stress/tests/forwarder/app.py
+++ t/stress/tests/forwarder/app.py
@@ -6,6 +6,7 @@ from ...app import create_stress_app
 
 counter_received = 0
 found_duplicates = 0
+found_gaps = 0
 
 app = create_stress_app(
     name='f-stress-dedupe',
@@ -27,6 +28,12 @@ app.add_system_check(
         get_value=lambda: found_duplicates,
     ),
 )
+app.add_system_check(
+    checks.Stationary(
+        'gaps',
+        get_value=lambda: found_gaps,
+    ),
+)
 
 partitions_sent_counter = Counter()
 
@@ -46,7 +53,7 @@ async def on_leader_send_monotonic_counter(app, max_latency=0.08) -> None:
                 if app.rebalancing:
                     break
                 current_value = partitions_sent_counter.get(partition, 0)
-                await check.send(value=current_value, partition=partition)
+                await process.send(value=current_value, partition=partition)
                 partitions_sent_counter[partition] += 1
             await app._service.sleep(random.uniform(0, max_latency))
         else:
@@ -56,6 +63,7 @@ async def on_leader_send_monotonic_counter(app, max_latency=0.08) -> None:
 @app.agent(value_type=int)
 async def process(numbers: Stream[int]) -> None:
     # Next agent reads from topic and forwards numbers to another agent
+    # We keep the partitioning
     async for event in numbers.events():
         await check.send(value=event.value, partition=event.message.partition)
 
@@ -66,6 +74,7 @@ async def check(forwarded_numbers: Stream[int]) -> None:
     # (repeating or decreasing numbers are evidence of duplicates).
     global counter_received
     global found_duplicates
+    global found_gaps
     value_by_partition = Counter()
     async for event in forwarded_numbers.events():
         number = event.value
@@ -76,9 +85,19 @@ async def check(forwarded_numbers: Stream[int]) -> None:
         if previous_number is not None:
             if number > 0:
                 # consider 0 as the service being restarted.
+
                 if number <= previous_number:
+                    # number should be larger than the previous number.
+                    # if that's not true it means we have a duplicate.
                     app.log.error('Found duplicate number in %r: %r',
                                   event.message.tp, number)
                     found_duplicates += 1
+                if number != previous_number + 1:
+                    # the number should also be exactly one up from the last
+                    # otherwise we may have dropped a number.
+                    app.log.error(
+                        'Found sequence gap for tp %r: this=%r previous=%r',
+                        event.message.tp, number, previous_number)
+                    found_gaps += 1
         value_by_partition[partition] = number
         counter_received += 1
