8158639a2f68bbcc671e345c2988be16203aa60e
Ask Solem <asksol@robinhood.com>
2018-11-08 15:46:11 -0800

    typecheck

--- faust/utils/iso8601.py
+++ faust/utils/iso8601.py
@@ -3,6 +3,6 @@ try:  # pragma: no cover
 except ImportError:  # pragma: no cover
     from ._iso8601_python import parse
 else:
-    parse = ciso8601.parse_datetime  # type: ignore
+    parse = ciso8601.parse_datetime
 
 __all__ = ['parse']
