bdf986d0f6b4e0bcf67401665ab9b051e777c197
Ask Solem <asksol@robinhood.com>
2019-02-15 16:27:40 -0800

    RocksDB: Removes debugging statement

--- faust/stores/rocksdb.py
+++ faust/stores/rocksdb.py
@@ -331,8 +331,6 @@ class Store(base.SerializedStore):
     def _visible_items(self, db: DB) -> Iterator[Tuple[bytes, bytes]]:
         it = db.iteritems()  # noqa: B301
         it.seek_to_first()
-        for i, x in enumerate(it):
-            print('IT %r %r' % (i, x))
         for key, value in it:
             if key != self.offset_key:
                 yield key, value
