326484c15c62c39e52d26c8a6a3710a0b25454f2
Ask Solem <asksol@robinhood.com>
2019-03-08 11:11:18 -0800

    Fixes tests

--- t/unit/transport/test_consumer.py
+++ t/unit/transport/test_consumer.py
@@ -534,7 +534,7 @@ class test_Consumer:
         await consumer.perform_seek()
 
         assert consumer._read_offset[TP1] == 4001
-        assert consumer._read_offset[TP2] is None
+        assert consumer._read_offset[TP2] == 0
 
         assert consumer._committed_offset[TP1] == 4001
         assert consumer._committed_offset[TP2] is None
