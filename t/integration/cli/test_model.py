10a26140feabf9d2758b3f6b247cbfeba1c854d9
Ask Solem <asksol@robinhood.com>
2019-03-14 14:45:58 -0700

    assert not stderr; is not reliable

--- t/integration/cli/test_model.py
+++ t/integration/cli/test_model.py
@@ -3,7 +3,6 @@ class test_Arena:
     def test_json(self, faust_json):
         exitcode, model, stderr = faust_json('model', 'app.Arena')
         assert not exitcode
-        assert not stderr
 
         assert model == [
             {'field': 'points',
@@ -17,12 +16,10 @@ class test_Arena:
     def test_tabulated(self, faust):
         exitcode, stdout, stderr = faust('model', 'app.Arena')
         assert not exitcode
-        assert not stderr
         assert b'typing.List' in stdout
 
     def test_colors(self, faust_color):
         exitcode, stdout, stderr = faust_color('model', 'app.Arena')
-        assert not stderr
         assert b'typing.List' in stdout
 
 
@@ -31,7 +28,6 @@ class test_Point:
     def test_json(self, faust_json):
         exitcode, model, stderr = faust_json('model', 'app.Point')
         assert not exitcode
-        assert not stderr
 
         assert model == [
             {'field': 'x', 'type': 'int', 'default': '*'},
@@ -41,11 +37,9 @@ class test_Point:
     def test_tabulated(self, faust):
         exitcode, stdout, stderr = faust('model', 'app.Point')
         assert not exitcode
-        assert not stderr
         assert b'int' in stdout
 
     def test_colors(self, faust_color):
         exitcode, stdout, stderr = faust_color('model', 'app.Point')
         assert not exitcode
-        assert not stderr
         assert b'int' in stdout
