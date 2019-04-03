cbea7fdb7f705668780f2539679a86faf6b2880d
Ask Solem <asksol@robinhood.com>
2019-04-03 14:53:22 -0700

    Monitor: Use deque and pop after append instead of periodic cleanup

---
+++ faust/livecheck/runners.py
@@ -0,0 +1,197 @@
+"""LiveCheck - Test runner."""
+import asyncio
+import logging
+import typing
+from time import monotonic
+from typing import (
+    Any,
+    Dict,
+    Iterable,
+    List,
+    Mapping,
+    Optional,
+    Tuple,
+)
+
+from mode.utils.logging import CompositeLogger
+from mode.utils.times import humanize_seconds
+from mode.utils.typing import NoReturn
+
+from .exceptions import (
+    LiveCheckError,
+    TestFailed,
+    TestRaised,
+    TestSkipped,
+    TestTimeout,
+)
+from .locals import current_test_stack
+from .models import State, TestExecution
+from .signals import BaseSignal
+from .utils import to_model
+
+if typing.TYPE_CHECKING:
+    from .case import Case as _Case
+else:
+    class _Case: ...  # noqa
+
+__all__ = ['TestRunner']
+
+
+class TestRunner:
+
+    case: _Case
+
+    state: State = State.INIT
+    test: TestExecution
+
+    started: float
+    ended: Optional[float]
+    runtime: Optional[float]
+
+    logs: List[Tuple[str, Tuple]]
+    signal_latency: Dict[str, float]
+
+    def __init__(self,
+                 case: _Case,
+                 test: TestExecution,
+                 started: float) -> None:
+        self.case = case
+        self.test = test
+        self.started = started
+        self.ended = None
+        self.runtime = None
+        self.logs = []
+        self.log = CompositeLogger(
+            self.case.logger,
+            formatter=self._format_log,
+        )
+        self.signal_latency = {}
+
+    async def execute(self) -> None:
+        test = self.test
+        with current_test_stack.push(test):
+            if not self.case.active:
+                await self.skip('case inactive')
+            elif test.is_expired:
+                await self.skip('expired')
+            args = self._prepare_args(test.test_args)
+            kwargs = self._prepare_kwargs(test.test_kwargs)
+            await self.on_start()
+            try:
+                await self.case.run(*args, **kwargs)
+            except asyncio.CancelledError:
+                pass
+            except TestSkipped as exc:
+                await self.on_skipped(exc)
+                raise
+            except TestTimeout as exc:
+                await self.on_timeout(exc)
+                raise
+            except AssertionError as exc:
+                await self.on_error(exc)
+                raise TestFailed(exc) from exc
+            except LiveCheckError as exc:
+                await self.on_error(exc)
+                raise
+            except Exception as exc:
+                await self.on_error(exc)
+                raise TestRaised(exc) from exc
+            else:
+                await self.on_pass()
+
+    async def skip(self, reason: str) -> NoReturn:
+        exc = TestSkipped(f'Test {self.test.ident} skipped: {reason}')
+        await self.on_skipped(exc)
+        raise exc
+
+    def _prepare_args(self, args: Iterable) -> Tuple:
+        to_value = self._prepare_val
+        return tuple(to_value(arg) for arg in args)
+
+    def _prepare_kwargs(self, kwargs: Mapping[str, Any]) -> Mapping[str, Any]:
+        to_value = self._prepare_val
+        return {
+            to_value(k): to_value(v)
+            for k, v in kwargs.items()
+        }
+
+    def _prepare_val(self, arg: Any) -> Any:
+        return to_model(arg)
+
+    def _format_log(self, severity: int, msg: str,
+                    *args: Any, **kwargs: Any) -> str:
+        return f'[{self.test.shortident}] {msg}'
+
+    async def on_skipped(self, exc: TestSkipped) -> None:
+        self.state = State.SKIP
+        self.log.info('Skipped expired test: %s expires=%s',
+                      self.test.ident, self.test.expires)
+        await self.case.on_test_skipped(self)
+
+    async def on_start(self) -> None:
+        self.log_info('≈≈≈ Test %s executing... (issued %s) ≈≈≈',
+                      self.case.name, self.test.human_date)
+        await self.case.on_test_start(self)
+
+    async def on_signal_wait(self, signal: BaseSignal, timeout: float) -> None:
+        self.log_info('∆ %r/%r %s (%rs)...',
+                      signal.index,
+                      self.case.total_signals,
+                      signal.name.upper(),
+                      timeout)
+
+    async def on_signal_received(self,
+                                 signal: BaseSignal,
+                                 time_start: float,
+                                 time_end: float) -> None:
+        latency = time_end - time_start
+        self.signal_latency[signal.name] = latency
+
+    async def on_failed(self, exc: BaseException) -> None:
+        self.end()
+        self.state = State.FAIL
+        self.log.exception('Test failed: %r', exc)
+        await self.case.on_test_failed(self, exc)
+
+    async def on_error(self, exc: BaseException) -> None:
+        self.end()
+        self.state = State.ERROR
+        self.log.exception('Test raised: %r', exc)
+        await self.case.on_test_error(self, exc)
+
+    async def on_timeout(self, exc: BaseException) -> None:
+        self.end()
+        self.state = State.TIMEOUT
+        self.log.exception('Test timed-out: %r', exc)
+        await self.case.on_test_timeout(self, exc)
+
+    async def on_pass(self) -> None:
+        self.end()
+        self.state = State.PASS
+        human_secs = humanize_seconds(
+            self.runtime,
+            microseconds=True,
+            now='~0.0 seconds',
+        )
+        self.log_info('Test OK in %s √', human_secs)
+        self._flush_logs()
+        await self.case.on_test_pass(self)
+
+    def log_info(self, msg: str, *args: Any) -> None:
+        if self.case.realtime_logs:
+            self.log.info(msg, *args)
+        else:
+            self.logs.append((msg, args))
+
+    def end(self) -> None:
+        self.ended = monotonic()
+        self.runtime = self.ended - self.started
+
+    def _flush_logs(self, severity: int = logging.INFO) -> None:
+        logs = self.logs
+        try:
+            self.log.log(severity, '\n'.join(
+                self._format_log(severity, msg % log_args)  # noqa: S001
+                for msg, log_args in logs))
+        finally:
+            logs.clear()
