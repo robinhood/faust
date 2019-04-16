82960855f9a71969314cff2e96bb76b0de6010ab
Ask Solem <asksol@robinhood.com>
2019-04-16 15:21:36 -0700

    Master is now 1.7.0a1

--- faust/__init__.py
+++ faust/__init__.py
@@ -24,7 +24,7 @@ import typing
 
 from typing import Any, Mapping, NamedTuple, Optional, Sequence, Tuple
 
-__version__ = '1.6.0a1'
+__version__ = '1.7.0a1'
 __author__ = 'Robinhood Markets, Inc.'
 __contact__ = 'contact@fauststream.com'
 __homepage__ = 'http://faust.readthedocs.io/'
