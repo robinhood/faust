743551dd20c7a3215101931a4e2f77a3792e3d79
Ask Solem <asksol@robinhood.com>
2019-04-16 15:20:15 -0700

    Revert "Monitor: Use deque and pop after append instead of periodic cleanup"
    This reverts commit 90eb67ccf8b5230d8fbc9083d92d489c088a34d2.

--- faust/utils/functional.py
+++ faust/utils/functional.py
@@ -1,11 +1,8 @@
 """Functional utilities."""
 from itertools import groupby
-from typing import Iterable, Iterator, Optional, Sequence, TypeVar
-from mode.utils.typing import Deque
+from typing import Iterable, Iterator, Sequence
 
-__all__ = ['consecutive_numbers', 'deque_prune', 'deque_pushpopmax']
-
-T = TypeVar('T')
+__all__ = ['consecutive_numbers']
 
 
 def consecutive_numbers(it: Iterable[int]) -> Iterator[Sequence[int]]:
@@ -16,18 +13,3 @@ def consecutive_numbers(it: Iterable[int]) -> Iterator[Sequence[int]]:
     """
     for _, g in groupby(enumerate(it), lambda a: a[0] - a[1]):
         yield [a[1] for a in g]
-
-
-def deque_prune(l: Deque[T], max: int = None) -> Optional[T]:
-    """Prune oldest element in deque if size exceeds ``max``."""
-    if max is not None:
-        size = len(l)
-        if size > max:
-            return l.popleft()
-    return None
-
-
-def deque_pushpopmax(l: Deque[T], item: T, max: int = None) -> Optional[T]:
-    """Append to deque and remove oldest element if size exceeeds ``max``."""
-    l.append(item)
-    return deque_prune(l, max)
