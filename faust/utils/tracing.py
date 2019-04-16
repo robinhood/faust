1e27a55c4e2803d7e0c0b2c41772cf2f011fbe9f
Ask Solem <asksol@robinhood.com>
2019-04-16 15:18:29 -0700

    Revert "Tracing: call_with_trace must return coroutine return value"
    This reverts commit 41004df412927b2d60688916018d1d03540d4dd4.

--- faust/utils/tracing.py
+++ faust/utils/tracing.py
@@ -114,9 +114,8 @@ def call_with_trace(span: opentracing.Span,
             # if async def method, we attach our span to
             # when it completes.
             async def corowrapped() -> Any:
-                await_ret = None
                 try:
-                    await_ret = await ret
+                    await ret
                 except BaseException:
                     span.__exit__(*sys.exc_info())
                     if cb:
@@ -126,7 +125,6 @@ def call_with_trace(span: opentracing.Span,
                     span.__exit__(None, None, None)
                     if cb:
                         cb(*cb_args)
-                    return await_ret
             return corowrapped()
         else:
             # for non async def method, we just exit the span.
