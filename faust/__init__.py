de91818d151bc49f7b3262468ea9a597f2526587
Ask Solem <asksol@robinhood.com>
2019-09-27 16:07:38 -0700

    Bump version: 1.7.3 → 1.8.0

--- faust/__init__.py
+++ faust/__init__.py
@@ -24,7 +24,7 @@ import typing
 
 from typing import Any, Mapping, NamedTuple, Optional, Sequence, Tuple
 
-__version__ = '1.7.3'
+__version__ = '1.8.0'
 __author__ = 'Robinhood Markets, Inc.'
 __contact__ = 'contact@fauststream.com'
 __homepage__ = 'http://faust.readthedocs.io/'
