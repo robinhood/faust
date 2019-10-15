a237d0fd4d754ec8bc08e54bc3c28a023bc22670
leandro <leandro.vonwerra@spoud.io>
2019-10-15 10:49:59 +0200

    style: replace double quotes with single quotes and minor improvements

--- examples/windowed_aggregation.py
+++ examples/windowed_aggregation.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 from time import time
 import random
 import faust
@@ -15,16 +15,16 @@ class AggModel(faust.Record):
     mean: float
 
 
-TOPIC = "raw-event"
-SINK = "agg-event"
-TABLE = "tumbling_table"
-KAFKA = "kafka://localhost:9092"
+TOPIC = 'raw-event'
+SINK = 'agg-event'
+TABLE = 'tumbling_table'
+KAFKA = 'kafka://localhost:9092'
 CLEANUP_INTERVAL = 1.0
 WINDOW = 10
 WINDOW_EXPIRES = 1
 PARTITIONS = 1
 
-app = faust.App("windowed-agg", broker=KAFKA, version=1, topic_partitions=1)
+app = faust.App('windowed-agg', broker=KAFKA, version=1, topic_partitions=1)
 
 app.conf.table_cleanup_interval = CLEANUP_INTERVAL
 source = app.topic(TOPIC, value_type=RawModel)
@@ -37,7 +37,10 @@ def window_processor(key, events):
     count = len(values)
     mean = sum(values) / count
 
-    print("processing window: %i events, mean: %.2f, timestamp %i" % (len(values), mean, timestamp))
+    print(
+        'processing window: %i events, mean: %.2f, timestamp %i'
+        % (len(values), mean, timestamp)
+    )
     sink.send_soon(value=AggModel(date=timestamp, count=count, mean=mean))
 
 
@@ -56,9 +59,9 @@ tumbling_table = (
 @app.agent(source)
 async def print_windowed_events(stream):
     async for event in stream:
-        value_list = tumbling_table["events"].value()
+        value_list = tumbling_table['events'].value()
         value_list.append(event)
-        tumbling_table["events"] = value_list
+        tumbling_table['events'] = value_list
 
 
 @app.timer(0.1)
@@ -66,5 +69,5 @@ async def produce():
     await source.send(value=RawModel(value=random.random(), date=int(time())))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.main()
