bb18bdb8591b907635736a4bc211a1ad98a80179
Josh Herrera <jherrera@domaintools.com>
2022-06-07 14:17:19 -0700

    updated dependency versions

--- faust/__init__.py
+++ faust/__init__.py
@@ -24,7 +24,7 @@ import typing
 
 from typing import Any, Mapping, NamedTuple, Optional, Sequence, Tuple
 
-__version__ = '1.12.9'
+__version__ = '1.12.10'
 __author__ = 'Robinhood Markets, Inc.'
 __contact__ = 'contact@fauststream.com'
 __homepage__ = 'http://faust.readthedocs.io/'
